a139338cb0d941a36f05340e54923ccc8cab0c6c
Aliaksei Kaliada <akaliada@exadel.com>
2021-06-14 17:21:14 +0300

    Initial commit

---
+++ core/src/main/java/com/exadel/kaliada/core/models/ByLineModel.java
@@ -0,0 +1,59 @@
+package com.exadel.kaliada.core.models;
+
+
+import com.adobe.cq.wcm.core.components.models.Image;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.Collections;
+import java.util.List;
+
+@Model(adaptables = SlingHttpServletRequest.class,
+       resourceType = ByLineModel.RESOURCE_TYPE,
+       defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class ByLineModel {
+    protected static final String RESOURCE_TYPE = "aemkaliada/components/byline";
+
+    @ValueMapValue
+    private String name;
+
+    @ValueMapValue
+    private List<String> occupations;
+
+    @Self
+    private Image image;
+
+    public String getName(){
+        return name;
+    }
+
+    public List<String> getOccupations(){
+        if (occupations != null){
+            Collections.sort(occupations);
+            return occupations;
+        }else {
+            return Collections.emptyList();
+        }
+    }
+
+    public boolean isEmpty(){
+        Image image = getImage();
+        if (StringUtils.isBlank(name)){
+            return true;
+        }else if (occupations == null || occupations.isEmpty()){
+            return true;
+        }else if (image == null || StringUtils.isBlank(image.getSrc())){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private Image getImage(){
+        return image;
+    }
+}
